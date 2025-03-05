4e6501a2cff7c5b1de6dffcd3d7224e014b5d737
josedvargas31 <dajozavargas@gmail.com>
2025-03-05 07:53:08 -0500

    Refactor Sidebar component to directly use reactive store properties for nombre and email

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,26 +7,26 @@
         <li class="list"><router-link to="/lista-de-tareas">Lista De Tareas</router-link></li>
         <li class="list"><router-link to="/registrar">Registrar</router-link></li>
       </ul>
-      <h3>Nombre: {{ nombre }}</h3>
-      <h3>Email: {{ email }}</h3>
+      <h3>Nombre: {{ registrarStore.nombre }}</h3> <!-- cuando imprimimos una variable reactiva, no es necesario el .value -->
+      <h3>Email: {{ registrarStore.email }}</h3>
     </nav>
   </div>
 </template>
 
 <script setup>
 import { useRegistrarStore } from '@/modules/registro/stores/registrarStore';
-import { ref, watch } from 'vue'
+// import { ref, watch } from 'vue'
 
 const registrarStore = useRegistrarStore();
-const nombre = ref(registrarStore.nombre);
-const email = ref(registrarStore.email);
+// const nombre = ref(registrarStore.nombre);
+// const email = ref(registrarStore.email);
 
-watch(() => registrarStore.nombre, (newValue) => {
-  nombre.value = newValue;
-});
-watch(() => registrarStore.email, (newValue) => {
-  email.value = newValue;
-});
+// watch(() => registrarStore.nombre, (newValue) => {
+//   nombre.value = newValue;
+// });
+// watch(() => registrarStore.email, (newValue) => {
+//   email.value = newValue;
+// });
 
 </script>
 
